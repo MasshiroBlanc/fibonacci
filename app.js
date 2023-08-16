d82439eb11d8639b082591f25dc33c77991b6fc9
masshiroBlanc <shirataki7447242@gmail.com>
2023-08-16 18:35:28 +0900

    指数オーダーのfib関数の実装

--- app.js
+++ app.js
@@ -1,1 +1,16 @@
 'use strict';
+function fib(n) {
+  if (n === 0) {
+    return 0;
+  }
+  else if (n === 1) {
+    return 1;
+  }
+
+  return fib(n - 1) + fib(n - 2);
+}
+
+const length = 40;
+for (let num = 0; num <= length; num++) {
+  console.log(`num: ${num}, result:${fib(num)}`);
+}
